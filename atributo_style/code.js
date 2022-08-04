4798fe07ec4214432c3aaf294b343d1a01aeaf40
Hugo <105953757+HugoLuquePerez@users.noreply.github.com>
2022-08-04 11:47:26 +0200

    Add files via upload

---
+++ atributo_style/code.js
@@ -0,0 +1,7 @@
+/* Atrihbuto style:
+Usando el "Element.style.PropiedadACambiar" podemos estlizar como queramos desde el codigo JS */
+
+const titulo = document.querySelector(".titulo");
+
+titulo.style.backgroundColor = "#32b"
+titulo.style.padding = "30px"
